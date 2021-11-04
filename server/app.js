d718a1c3d13fc632cf83ebca713fef9f3f7021ab
Eng-Mohamed-Osama <eng.muhamed.osama@gmail.com>
2021-11-04 10:16:06 +0400

    adding The Swagger documentation for the api

--- server/app.js
+++ server/app.js
@@ -5,6 +5,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//swagger config
+var path = require("path");
+var swagger_path = path.resolve(__dirname, "./swagger/swagger.yaml");
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load(swagger_path);
+
 //port
 const port = process.env.PORT || 3000;
 
@@ -21,6 +28,15 @@ const { notFound } = require("./middleware/not-found");
 //middleware
 app.use(express.static("./public"));
 app.use(express.json());
+
+//documentation route
+app.get("/docs", (req, res) => {
+  return res.send(
+    "<h1 style='color:gray'>Task Api</h1> <span>click here </span><a href='/api-docs' style='color:red;font-size:16px; font-weight:bold ;text-decoration:none;'>Documentation</a> <span>for the full api documentation</span>"
+  );
+});
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 //routes
 app.use("/api/v1/tasks/", tasksRouter);
